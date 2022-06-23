5b4bca6cb3f23796ed80e5d347d61ad4e8e9b314
Bruno Vinícius <brunooliver368@gmail.com>
2022-06-23 16:14:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import Avatar from './assets/avatar.svg'
+import LinkedinImage from './assets/linkedin.svg'
+import InstagramImage from './assets/instagram.svg'
+import PortfolioImage from './assets/portifólio.svg'
+import GithubImage from './assets/github.svg'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <img src={Avatar} alt='avatar' />
+      <h1> Bruno Vinícius </h1>
+      <p> Full Stack <span> Developer </span> </p>
+      <div className='container-button1'>
+       <a href='https://www.linkedin.com/in/brunobarreiras/' target='_blank'> <button> <img src={LinkedinImage} alt='linkedin' /> LinkedIn </button> </a>
+       <a href='https://www.instagram.com/brunowzz/' target='_blank'> <button> <img src={InstagramImage} alt='instagram' /> Instagram </button> </a>
+      </div>
+      <div className='container-button2'>
+       <a href='#'> <button> <img src={PortfolioImage} alt='portfolio' /> Portfólio </button> </a>
+       <a href='https://github.com/brunowzz' target='_blank'> <button> <img src={GithubImage} alt='github' /> GitHub </button> </a>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
