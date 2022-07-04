1a9dee06e6fec5b807ab6a810ebb19f27f021f32
Bruno Vinícius <brunooliver368@gmail.com>
2022-07-04 12:31:56 -0300

    new version

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,15 @@ function App() {
       <img src={Avatar} alt='avatar' />
       <h1> Bruno Vinícius </h1>
       <p> Full Stack <span> Developer </span> </p>
-      <div className='container-button1'>
-       <a href='https://www.linkedin.com/in/brunobarreiras/' target='_blank'> <button> <img src={LinkedinImage} alt='linkedin' /> LinkedIn </button> </a>
-       <a href='https://www.instagram.com/brunowzz/' target='_blank'> <button> <img src={InstagramImage} alt='instagram' /> Instagram </button> </a>
-      </div>
-      <div className='container-button2'>
-       <a href='#'> <button> <img src={PortfolioImage} alt='portfolio' /> Portfólio </button> </a>
-       <a href='https://github.com/brunowzz' target='_blank'> <button> <img src={GithubImage} alt='github' /> GitHub </button> </a>
+      <div className='container-buttons'>
+        <div className='container-button1'>
+          <a href='https://www.linkedin.com/in/brunobarreiras/' target='_blank'> <button className='linkedin button'> <img src={LinkedinImage} alt='linkedin' /> LinkedIn </button> </a>
+          <a href='https://www.instagram.com/brunowzz/' target='_blank'> <button className='instagram button'> <img src={InstagramImage} alt='instagram' /> Instagram </button> </a>
+        </div>
+        <div className='container-button2'>
+          <a href='#'> <button className='portfolio button'> <img src={PortfolioImage} alt='portfolio' /> Portfólio </button> </a>
+          <a href='https://github.com/brunowzz' target='_blank'> <button className='github button'> <img src={GithubImage} alt='github' /> GitHub </button> </a>
+        </div>
       </div>
 
     </div>
